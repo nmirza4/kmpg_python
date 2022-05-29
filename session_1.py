67f2fdd55e13d0c0a314a3ac38dc1d3ebb5d490f
Nadia Mirza <nadia@nadias-air.home>
2022-05-29 10:48:09 +0100

    added first session

---
+++ session_1.py
@@ -0,0 +1,35 @@
+
+# # Section A
+# # 1. Write code that prints ‘Hello world’.
+print("Hello, world")
+# # 2. Print the numbers 1 to 5 on a single line.
+print(1,2,3,4,5)
+# # 3. Write a script where ‘Hello’ and ‘World’ are printed on two separate lines.
+print("Hello\nWorld")
+# # 4. Write a script that prints a list of names, tabbed on separate lines, e.g.
+# # My List of Names:
+# #             Alice
+# #             Bob
+# #             Charlie
+
+print("List of Names:\n\tAlice\n\tBob\n\tCharlie")
+
+# Section B
+# 1. Write code that prints the value of 2 + 2.
+number = 2+2
+print(number)
+# 2. Write code that prints the value of 5.7 subtracted from 3.4.
+number=3.4-5.7
+print(number)
+# 3. Write code that prints the value of 8 multiplied by 7.
+number=8*7
+print(number)
+# 4. Write code that prints the value of 144 divided by 12.
+number= 144/12
+print(number)
+# 5. Write code that prints the value of the remainder of 67 divided by 12.
+number=67%12
+print(number)
+# 6. Write code that finds the value of 20 from `4 - 2 * 6 / 3 * 5`. Hint: you might need brackets.
+# print(4-2) * (6/3)*5)
+print(2.3+6.7)
